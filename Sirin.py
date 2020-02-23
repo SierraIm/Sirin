61dd6a1559b4e07ca0eca953aa8ad9496877c5da
SierraIm <48466185+SierraIm@users.noreply.github.com>
2020-02-23 17:20:40 +0800

    Update Sirin.py

--- Sirin.py
+++ Sirin.py
@@ -8,7 +8,7 @@ bot = telepot.Bot('879508978:AAFr1ngABRI9vP2r2Jl9K58hlCwn4vPinAc')
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print(msg)
+    print(telepot.glance(msg))
     
     if msg['text']=='/ping':
         bot.sendMessage(chat_id,'boom!')
