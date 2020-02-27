faab70db61598c2f88a7e22b10010cddb98201bc
SierraIm <48466185+SierraIm@users.noreply.github.com>
2020-02-27 21:13:25 +0800

    Update Sirin.py

--- Sirin.py
+++ Sirin.py
@@ -23,7 +23,7 @@ def handle(msg):
         bot.sendMessage(chat_id,msg)
         
     if msg['text']=='/boom':
-        response=msg['reply_to_message']['chat']['first_name']+msg['reply_to_message']['chat']['last_name']+' boom!'
+        response=msg['reply_to_message']['from']['first_name']+msg['reply_to_message']['from']['last_name']+' boom!'
         bot.sendMessage(chat_id,response)
 
 
