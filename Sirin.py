68af89c84f7409ddff91aff792fab06fc170a304
SierraIm <48466185+SierraIm@users.noreply.github.com>
2020-02-27 21:18:15 +0800

    Update Sirin.py

--- Sirin.py
+++ Sirin.py
@@ -11,7 +11,7 @@ pp = pprint.PrettyPrinter(indent=4)
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    pp.pprint(msg,/n)
+    pp.pprint(msg+/n)
     
     if msg['text']=='/ping':
         bot.sendMessage(chat_id,'boom!')
