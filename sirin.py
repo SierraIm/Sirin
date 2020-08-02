9148f44d0fee34a2672b754012a2864b486204bf
SierraIm <48466185+SierraIm@users.noreply.github.com>
2020-08-03 01:16:18 +0800

    Update sirin.py

--- sirin.py
+++ sirin.py
@@ -5,12 +5,19 @@
 import telegram
 import time
 from telegram.ext import Updater
+from telegram.ext import CommandHandler
 
 tk = '879508978:AAFr1ngABRI9vP2r2Jl9K58hlCwn4vPinAc'
 
 bot = telegram.Bot(token=tk)
 updater = Updater(token=tk)
 dispatcher = updater.dispatcher
+
+def yurui(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="boom!")
+    
+boom = CommandHandler('yurui', yurui)
+dispatcher.add_handler(boom)    
 
 while(1):
     time.sleep(10)
