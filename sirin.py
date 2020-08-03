86e2beac23ff002e835f91fada016410759ec086
SierraIm <48466185+SierraIm@users.noreply.github.com>
2020-08-04 00:07:18 +0800

    add

--- sirin.py
+++ sirin.py
@@ -23,15 +23,20 @@ updater = Updater(token=tk, request_kwargs={'proxy_url': 'http://127.0.0.1:10809
 
 dispatcher = updater.dispatcher
 
+def start(update,context):
+    context.bot.sendPhoto(chat_id=update.effective_chat.id,photo=open('resource/0_error.jpg', 'rb'))
+    
 def yurui(update,context):
     context.bot.sendMessage(chat_id=update.effective_chat.id, text="boom!")
     
 def yurui_test(update,context):
     context.bot.sendSticker(chat_id=update.effective_chat.id,sticker="CAACAgUAAxkBAAIBQV8nvdnbQqb1NLH2Z0PjEH9xvO2qAAJYAAPFBioILE-pYyaFaRYaBA")
     
+start = CommandHandler('start',start)
 boom = CommandHandler('yurui', yurui)
 yuruiTest = CommandHandler('yurui_test',yurui_test)
 
+dispatcher.add_handler(start)
 dispatcher.add_handler(boom)   
 dispatcher.add_handler(yuruiTest) 
 
