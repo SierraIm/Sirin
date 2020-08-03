dc28073a5573b687e2f5b199c648c49da2c8a442
SierraIm <48466185+SierraIm@users.noreply.github.com>
2020-08-03 15:22:44 +0800

    Update sirin.py

--- sirin.py
+++ sirin.py
@@ -4,8 +4,7 @@
 
 import telegram
 import time
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+import telegram.ext
 
 tk = '879508978:AAFr1ngABRI9vP2r2Jl9K58hlCwn4vPinAc'
 
