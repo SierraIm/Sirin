04aac6e7d9167db24e6a4bcbba19c4682b76f026
SierraIm <48466185+SierraIm@users.noreply.github.com>
2020-10-23 13:31:58 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const TelegramBot = require('node-telegram-bot-api')
+const token = '1139441431:AAGK1mff_XD1rRHqSNWsskRjMNWVmSFtH_w'
+const bot = new TelegramBot(token,{pplling:true});
+
+bot.onText(/\/echo(.+)/,(msg,match) +>{
+	const chatId = msg.chat.id;
+	const resp = match[1];
+	bot.sendMessage(chatId,resp);
+})
+
+bot.on('message',(msg) => {
+	const chatId = msg.chat.id;
+	bot.sendMessage(chatId,'pong');
+})
